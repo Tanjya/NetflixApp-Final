ab1b4534c838ae61edc1d2a1a3a8546cdfc29cbf
Tanjya <tanjya26@gmail.com>
2023-11-23 20:37:34 +0000

    background black

--- client/src/pages/MovieList.tsx
+++ client/src/pages/MovieList.tsx
@@ -1,32 +1,30 @@
 import MovieCard from "../components/MovieCard";
 import { Movie } from "../types";
 
-const MovieList = (
-    { movies,
-        lastElementRef }: {
-            movies: Movie[];
-            lastElementRef: (node: HTMLDivElement) => void;
-        }) => {
-    return (
-        <div className="px-12 mt-4 space-y-8">
-            <div>
-                <p className="text-black text-2xl font-semibold mb-4">
-                    Popular Show
-                </p>
+const MovieList = ({
+  movies,
+  lastElementRef,
+}: {
+  movies: Movie[];
+  lastElementRef: (node: HTMLDivElement) => void;
+}) => {
+  return (
+    <div className="px-12 mt-4 space-y-8 bg-black">
+      <div>
+        <p className="text-white text-2xl font-semibold mb-4">Popular Show</p>
 
-                <div className="flex flex-wrap gap-2 justify-between">
-                    {movies.map((movie, ind) => (<MovieCard
-                        key={movie.id}
-                        movie={movie}
-                        lastElementRef={movies.length === ind + 1 ? lastElementRef : null}
-                    />))
-                    }
-
-                </div>
-            </div>
-
+        <div className="flex flex-wrap gap-2 justify-between">
+          {movies.map((movie, ind) => (
+            <MovieCard
+              key={movie.id}
+              movie={movie}
+              lastElementRef={movies.length === ind + 1 ? lastElementRef : null}
+            />
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default MovieList;
